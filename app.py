fd7de4655a6866f98c9e8a3928b987603e53d87e
sammac-student <s3888492@student.rmit.edu.au>
2021-11-10 19:26:51 +1100

    test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+# commit testing
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # page is loaded, need to simply display the form
